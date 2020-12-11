f7aa0a8c14f4a6989e8fb64b51917e94927c504d
Thiago Gomes <thiagomello1999@hotmail.com>
2020-12-11 09:55:09 -0300

    initial commit

---
+++ banco/src/br/solutis/banco/BancoExecute.java
@@ -0,0 +1,7 @@
+package br.solutis.banco;
+
+public class BancoExecute {
+	public static void main(String[] args) {
+		System.out.println("Isso vai virar um banco. Será?");
+	}
+}
---
+++ hello/src/br/solutis/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package br.solutis.hello;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+	}
+}
